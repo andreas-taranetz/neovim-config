51f5a237a2ebe155e1113b0c35a950921ff6be43
Andreas Taranetz <57600104+holistic-developer@users.noreply.github.com>
2025-11-16 00:47:58 +0100

    Customize snacks config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,38 @@
+return {
+	"snacks.nvim",
+	opts = {
+		dashboard = {
+			preset = {
+				header = [[
+              ███                 
+             ░░░                  
+ █████ █████ ████  █████████████  
+░░███ ░░███ ░░███ ░░███░░███░░███ 
+ ░███  ░███  ░███  ░███ ░███ ░███ 
+ ░░███ ███   ░███  ░███ ░███ ░███ 
+  ░░█████    █████ █████░███ █████
+   ░░░░░    ░░░░░ ░░░░░ ░░░ ░░░░░ 
+        ]],
+			},
+			sections = {
+				{ section = "header" },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
+				{ icon = " ", title = "Bookmarks", section = "keys", padding = 1 },
+				{
+					icon = " ",
+					desc = "Browse Repo",
+					padding = 1,
+					key = "b",
+					action = function()
+						Snacks.gitbrowse()
+					end,
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+				},
+				{ section = "startup" },
+			},
+		},
+	},
+}
