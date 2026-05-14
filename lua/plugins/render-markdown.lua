c2fd2bef8e416586adce02a18bf89bbf0bdabab6
Andreas Taranetz <andreas.taranetz@dynatrace.com>
2026-05-14 23:00:17 +0200

    Remove markdown extra and re-add render-markdown plugin with snacks config to toggle

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,17 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
+	config = function(_, opts)
+		require("render-markdown").setup(opts)
+		Snacks.toggle({
+			name = "Render Markdown",
+			get = require("render-markdown").get,
+			set = require("render-markdown").set,
+		}):map("<leader>um")
+	end,
+}
