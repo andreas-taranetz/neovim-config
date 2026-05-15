5500b66273e1b41db3ada7cfd6daeeeeaf222a40
Andreas Taranetz <andreas.taranetz@dynatrace.com>
2026-05-15 12:03:02 +0200

    Disable latex support in render markdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -5,7 +5,11 @@ return {
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		latex = {
+			enabled = false,
+		},
+	},
 	config = function(_, opts)
 		require("render-markdown").setup(opts)
 		Snacks.toggle({
